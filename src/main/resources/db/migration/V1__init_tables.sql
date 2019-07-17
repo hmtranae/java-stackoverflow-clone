245973a1d78f0d683114b3d1a75acb3a7fcb4fb0
Hieu Tran <hmtran.ae@gmail.com>
2019-07-16 20:40:47 -0400

    answers table migration

--- src/main/resources/db/migration/V1__init_tables.sql
+++ src/main/resources/db/migration/V1__init_tables.sql
@@ -1,6 +1,7 @@
-CREATE TABLE questions (
-  id SERIAL PRIMARY KEY NOT NULL,
-  title VARCHAR(255) NOT NULL,
-  description text,
-  created_at TIMESTAMP NOT NULL DEFAULT NOW()
+CREATE TABLE questions
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description text,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
--- src/main/resources/db/migration/V2__answer_table.sql
+++ src/main/resources/db/migration/V2__answer_table.sql
@@ -1,2 +1,8 @@
 
 -- todo: do answer migration
+CREATE TABLE answers
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    description TEXT NOT NULL,
+    question_id INTEGER NOT NULL REFERENCES questions(id),
+);
