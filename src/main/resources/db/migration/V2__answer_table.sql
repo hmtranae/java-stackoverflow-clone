50e92a2572b1e0d231d755a60175d3187cd76b93
Hieu Tran <hmtran.ae@gmail.com>
2019-07-16 21:18:44 -0400

    fixed sql stray comma

--- src/main/resources/db/migration/V2__answer_table.sql
+++ src/main/resources/db/migration/V2__answer_table.sql
@@ -1,8 +1,6 @@
-
--- todo: do answer migration
 CREATE TABLE answers
 (
     id SERIAL PRIMARY KEY NOT NULL,
     description TEXT NOT NULL,
-    question_id INTEGER NOT NULL REFERENCES questions(id),
+    question_id INTEGER NOT NULL REFERENCES questions(id)
 );
